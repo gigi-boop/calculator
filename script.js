78beed4599dfdcde82da0d74a82938163f241918
gigi-boop <agentile89@protonmail.com>
2023-02-04 19:54:35 -0500

    Add event listeners to numbers to populate display

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 "use strict";
 
+const numberButtons = document.querySelectorAll(".number");
+
+const display = document.querySelector(".display");
+
 function add(a, b) {
     return a + b;
 }
@@ -19,3 +23,13 @@ function divide(a, b) {
 function operate(operation, a, b) {
     return operation(a, b);
 }
+
+function populateDisplay(buttonText) {
+    display.textContent += buttonText;
+}
+
+numberButtons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+        populateDisplay(event.target.textContent);
+    });
+});
